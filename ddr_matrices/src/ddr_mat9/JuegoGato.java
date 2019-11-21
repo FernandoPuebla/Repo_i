bf6bab1da3bf124a27b0c0c443f0f5743d48b66b
FernandoPuebla <fernando.puebla@4thsource.com>
2019-11-21 17:04:00 -0600

    ddr_mat9 casi lista, problema con que nunca termina

--- ddr_matrices/src/ddr_mat9/JuegoGato.java
+++ ddr_matrices/src/ddr_mat9/JuegoGato.java
@@ -4,96 +4,222 @@ import java.util.Scanner;
 
 public class JuegoGato
 {
+    private final static char CRUZ = 'x',CIRC = 'o', DEF = '-';
     private static char gato[][] = new char[3][3];
-    private static char cruz = 'X', circ = 'O', def = '-';
+    private static char jug1,jug2;
+    private static int c = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         crearTablero();
-        marcar();
-        contarFilas();
-        contarCols();
-        marcar();
-        contarFilas();
-        contarCols();
-        marcar();
-        contarFilas();
-        contarCols();
+        elegirJugador();
+        for (int i = 0;i <= 4;i++)
+        {
+            marcar(jug1);
+            contarFilas();
+            contarCols();
+            contarDiagPrin();
+            contarDiagInv();
+            marcar(jug2);
+            contarFilas();
+            contarCols();
+            contarDiagPrin();
+            contarDiagInv();
+        }
     }
-
-
 
     public static void crearTablero()
     {
-        for (int i = 0; i < 3;i++)
-        {
-            for (int j = 0; j < 3;j++)
-            {
-                gato[i][j] = def;
-                System.out.print(gato[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
-    }
-
-    public static void marcar()
-    {
-        int fil = 0, col = 0;
-        char marca;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Seleccione donde desea colocar X");
-        System.out.print("Fila ");
-        fil = sc.nextInt();
-        System.out.print("Columna ");
-        col = sc.nextInt();
-        gato[fil][col] = 'X';
-        for (int i = 0; i < 3;i++)
-        {
-            for (int j = 0; j < 3;j++)
-            {
-                System.out.print(gato[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
-
-    }
-
-    public static void contarFilas()
-    {
-        int cont = 0;
         for (int i = 0; i < gato.length;i++)
         {
             for (int j = 0; j < gato[0].length;j++)
             {
-                if(gato[i][j] == 'X')
+                gato[i][j] = DEF;
+                System.out.print(gato[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void elegirJugador()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elija una marca jugador 1: x / o");
+        jug1 = sc.next().charAt(0);
+        while(jug1 != CRUZ && jug1 != 'X' && jug1 != CIRC && jug1 != 'O')
+        {
+            System.out.println("Ingrese una marca válida, x | o");
+            jug1 = sc.next().charAt(0);
+        }
+        if(jug1 == CRUZ || jug1 == 'X')
+        {
+            System.out.println("El jugador 1 eligió: " + jug1);
+            jug2 = CIRC;
+            System.out.println("El jugador 2 es: " + jug2);
+        }else if(jug1 == CIRC || jug1 == 'O')
+        {
+            System.out.println("El jugador 1 eligió: " + jug1);
+            jug2 = CRUZ;
+            System.out.println("El jugador 2 es: " + jug2);
+        }
+    }
+    public static void marcar(int jug)
+    {
+        Scanner sc = new Scanner(System.in);
+        int fil = 0, col = 0;
+        char marca;
+        System.out.println("Seleccione donde desea colocar su marca, las filas y columnas se cuentan de izq a der y de arriba a abajo empezando por 1");
+        System.out.print("Fila ");
+        fil = (sc.nextInt() - 1);
+        while(fil < 0)
+        {
+            System.out.println("Ingrese un valor mayor que 0");
+            fil = (sc.nextInt() - 1);
+        }
+        System.out.print("Columna ");
+        col = (sc.nextInt() - 1);
+        while(col < 0)
+        {
+            System.out.println("Ingrese un valor mayor que 0");
+            col = (sc.nextInt() - 1);
+        }
+
+        if (jug == CRUZ)
+        {
+            while(gato[fil][col] == CIRC || gato[fil][col] == CRUZ)
+            {
+                System.out.println("Fila ocupada, escoga otra fila");System.out.println("Seleccione donde desea colocar su marca, las filas y columnas se cuentan de izq a der y de arriba a abajo empezando por 1");
+                System.out.print("Fila ");
+                fil = (sc.nextInt() - 1);
+                while(fil < 0)
                 {
-                    cont++;
+                    System.out.println("Ingrese un valor mayor que 0");
+                    fil = (sc.nextInt() - 1);
                 }
-            }if(cont == 3)
+                System.out.print("Columna ");
+                col = (sc.nextInt() - 1);
+                while(col < 0)
                 {
-            System.out.println("Ganaste " + cont);
+                    System.out.println("Ingrese un valor mayor que 0");
+                    col = (sc.nextInt() - 1);
+                }
+            }
+            gato[fil][col] = CRUZ;
+        }
+        else  if (jug == CIRC)
+        {
+                while(gato[fil][col] == CIRC || gato[fil][col] == CRUZ)
+                {
+                    System.out.println("Fila ocupada, escoga otra fila");System.out.println("Seleccione donde desea colocar su marca, las filas y columnas se cuentan de izq a der y de arriba a abajo empezando por 1");
+                    System.out.print("Fila ");
+                    fil = (sc.nextInt() - 1);
+                    while(fil < 0)
+                    {
+                        System.out.println("Ingrese un valor mayor que 0");
+                        fil = (sc.nextInt() - 1);
+                    }
+                    System.out.print("Columna ");
+                    col = (sc.nextInt() - 1);
+                    while(col < 0)
+                    {
+                        System.out.println("Ingrese un valor mayor que 0");
+                        col = (sc.nextInt() - 1);
+                    }
+                }
+            gato[fil][col] = CIRC;
+        }
+        for (int i = 0; i < gato.length;i++)
+        {
+            for (int j = 0; j < gato[0].length;j++)
+            {
+                System.out.print(gato[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void contarFilas()
+    {
+        int contCruz = 0, contCirc = 0;
+        for (int i = 0; i < gato.length;i++)
+        {
+            for (int j = 0; j < gato[0].length;j++)
+            {
+                if(gato[i][j] == CRUZ)
+                {
+                    contCruz++;
+                }
+                else if(gato[i][j] == CIRC)
+                {
+                    contCirc++;
+                }
+            }if(contCruz == 3)
+                {
+            System.out.println("Ganaste X");
             break;
-                }
-            else cont = 0;
-        }System.out.println("Hay " + cont +" Xs");
+                }else if(contCirc == 3)
+        {
+            System.out.println("Ganaste O");
+            break;
+        }
+            else {contCruz = 0;contCirc = 0;}
+        }
     }
     public static void contarCols()
     {
-        int cont = 0;
+        int contCruz = 0, contCirc = 0;
         for (int i = 0; i < gato.length;i++)
         {
             for (int j = 0; j < gato[0].length;j++)
             {
-                if(gato[j][i] == 'X')
+                if(gato[j][i] == CRUZ)
                 {
-                    cont++;
+                    contCruz++;
                 }
-            }if(cont == 3)
+                else if(gato[j][i] == CIRC)
+                {
+                    contCirc++;
+                }
+            }if(contCruz == 3)
         {
-            System.out.println("Ganaste " + cont);
+            System.out.println("Ganaste " + contCruz);
             break;
+        }else if(contCirc == 3)
+            {
+                System.out.println("Ganaste " + contCirc);
+                break;
+            }
+        else {contCruz = 0;contCirc = 0;}
         }
-        else cont = 0;
-        }System.out.println("Hay " + cont +" Xs");
+    }
+    public static void contarDiagPrin() {
+        int contCruz = 0, contCirc = 0;
+        for (int i = 0; i < gato.length; i++) {
+            if (gato[i][i] == CRUZ) {
+                contCruz++;
+            } else if (gato[i][i] == CIRC) {
+                contCirc++;
+            }
+        }if (contCruz == 3) {
+            System.out.println("Ganaste " + contCruz);
+        }else  if (contCirc == 3) {
+            System.out.println("Ganaste " + contCirc);
+        }
+        else {contCruz = 0;contCirc = 0;}
+    }
+    public static void contarDiagInv() {
+        int contCruz = 0,contCirc = 0, j = (gato.length-1);
+        for (int i = 0; i < gato.length; i++) {
+            if (gato[i][j] == CRUZ) {
+                contCruz++;
+            }else if (gato[i][j] == CIRC) {
+                contCirc++;
+            }
+                j--;
+
+        }if (contCruz == 3) {
+            System.out.println("Ganaste " + contCruz);
+        }else if (contCirc == 3) {
+            System.out.println("Ganaste " + contCirc);
+        } else {contCruz = 0;contCirc = 0;}
     }
 
 }
